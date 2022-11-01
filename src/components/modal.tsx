0eae78f6f3f4d71d160a4dc9c5c8ded3052af62b
daniel lambarri <dalambarri@gmail.com>
2022-10-31 23:37:33 -0300

    render error on cancel

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -58,6 +58,7 @@ const Modal = forwardRef(function Modal ({ onClose, addTodoList, controls, onRep
         onClick={() => {
           onClose()
           setItem('')
+          if (repeatItemError) return
           setRepeatItemError(false)
         }}
         >cancel
